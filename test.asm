a1a29ae09ac98174ab3127340d6aad67d55ad094
apelman <peyton.fraser@icloud.com>
2026-03-15 21:38:12 -0400

    new examle bin file

--- test.asm
+++ test.asm
@@ -3,7 +3,7 @@ bldsc
 rect 1 20 20 50 50 
 print
 
-A = 5
+A = 13
 func move
 movy 0 1
 upd
@@ -11,7 +11,5 @@ print
 endf move
 
 rep: A move
-
-savsc
 save
 # appears to not actually move the object for movx, but movy works
